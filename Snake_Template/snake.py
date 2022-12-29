fc0358ed720bfc9f5a7afde7371c0d752c18dc50
joeciminelli <86270978+joeciminelli@users.noreply.github.com>
2022-12-29 15:20:06 -0800

    Add files via upload

---
+++ Snake_Template/snake.py
@@ -0,0 +1,31 @@
+import pygame,sys,random
+from pygame.math import Vector2
+
+class FRUIT:
+	def __init__(self):
+		# Create x and y position
+		# Draw a Square Somewhere on the Grid with a Vector
+
+	def draw_fruit(self):
+		# create rectangle in right pos
+		# Draw it
+
+
+
+
+
+
+
+pygame.init()
+# Fix screen to be more flexible to our grid
+screen = pygame.display.set_mode((400,500))
+clock = pygame.time.Clock()
+
+# Initialize game loop like we did in Space Invaders
+
+	# Initialize loop within game loop for event management
+
+
+	# Create background color
+	pygame.display.update()
+	clock.tick(60)
